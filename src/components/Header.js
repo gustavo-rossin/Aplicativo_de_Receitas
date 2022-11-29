b44c549b95a75a33bb492b0cea65fe08f0bcbdfa
Miguel Soares <miguelanjohps@gmail.com>
2022-11-29 17:26:39 -0300

    feat: Adiciona requisito 7 e 8
    Cria um Header com um título para cada página e botão para redirecionar ao perfil
    
    Miguel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
-function Header() {
+function Header({ pageTitle, displaySearch }) {
+  const history = useHistory();
+
   return (
-    <div>Header</div>
+    <div>
+
+      <button
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Profile Button"
+        />
+      </button>
+      {
+        displaySearch && (
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="Search Button"
+          />
+        )
+      }
+      <h3
+        data-testid="page-title"
+      >
+        { pageTitle }
+      </h3>
+    </div>
   );
 }
+
+Header.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  displaySearch: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  displaySearch: false,
+};
 
 export default Header;
