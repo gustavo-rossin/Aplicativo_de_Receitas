81be36f472d1ed066413ebbd048500756c24ec1b
Miguel Soares <miguelanjohps@gmail.com>
2022-11-29 18:00:41 -0300

    feat: Adiciona requisito 9
    Implementa funcionalidade de mostrar e esconder barra de pesquisa ao clicar no ícone de pesquisa
    
    Miguel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header({ pageTitle, displaySearch }) {
   const history = useHistory();
+  const [isSearching, setIsSearching] = useState(false);
 
   return (
     <div>
-
-      <button
-        type="button"
-        onClick={ () => history.push('/profile') }
-      >
-        <img
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt="Profile Button"
-        />
-      </button>
-      {
-        displaySearch && (
+      <div>
+        {
+          displaySearch && (
+            <button
+              type="button"
+              onClick={ () => setIsSearching(!isSearching) }
+            >
+              <img
+                data-testid="search-top-btn"
+                src={ searchIcon }
+                alt="Search Button"
+              />
+            </button>
+          )
+        }
+        <button
+          type="button"
+          onClick={ () => history.push('/profile') }
+        >
           <img
-            data-testid="search-top-btn"
-            src={ searchIcon }
-            alt="Search Button"
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="Profile Button"
           />
-        )
-      }
-      <h3
-        data-testid="page-title"
-      >
-        { pageTitle }
-      </h3>
+        </button>
+        <h3
+          data-testid="page-title"
+        >
+          { pageTitle }
+        </h3>
+      </div>
+      <div>
+        { isSearching && <SearchBar />}
+      </div>
     </div>
   );
 }
 
 Header.propTypes = {
-  pageTitle: PropTypes.string.isRequired,
+  pageTitle: PropTypes.string,
   displaySearch: PropTypes.bool,
 };
 
 Header.defaultProps = {
+  pageTitle: '',
   displaySearch: false,
 };
 
