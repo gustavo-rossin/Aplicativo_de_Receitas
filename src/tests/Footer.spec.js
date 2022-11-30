d33cb0851c468722a5e517ec6d7e47d1b1d98cfe
Erick Argollo <argolloerickdev@gmail.com>
2022-11-29 21:06:21 -0300

    fix: footer test

--- src/tests/Footer.spec.js
+++ src/tests/Footer.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Recipes from '../pages/Recipes';
+import Footer from '../components/Footer';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Testes do componente Footer', () => {
   it('Teste de integração', () => {
-    renderWithRouter(<Recipes />);
+    renderWithRouter(<Footer />);
     const drinkIcon = screen.getByTestId('drinks-bottom-btn');
     expect(drinkIcon).toBeInTheDocument();
     const mealIcon = screen.getByTestId('meals-bottom-btn');
