768d3be984a3b4c06eda2816234359c6ad9c7482
Erick Argollo <argolloerickdev@gmail.com>
2022-11-30 16:20:49 -0300

    feat: add req 24 - 25 Obs: ajustar embed youtube video Erick e Gustavo

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import MealsContext from '../context/MealsContext';
+import mealApi from '../services/MealDbApi';
+import drinkApi from '../services/CockTailDbApi';
+import DetailsMealsDrinks from '../components/DetailsMealsDrinks';
 
 function RecipeDetails() {
+  const { apiResponse, setApiResponse } = useContext(MealsContext);
+
+  const history = useHistory();
+
+  console.log(apiResponse);
+
+  const recipeId = history.location.pathname.split('/')[2];
+
+  const pageTitle = history.location.pathname.includes('meals') ? 'Meals' : 'Drinks';
+
+  const searchId = async () => {
+    if (pageTitle === 'Meals') {
+      const response = await mealApi(recipeId, 'recipe-detail');
+      setApiResponse(response.meals);
+    } else {
+      const response = await drinkApi(recipeId, 'recipe-detail');
+      setApiResponse(response.drinks);
+    }
+  };
+
+  useEffect(() => {
+    searchId();
+  }, []);
+
   return (
-    <div>RecipeDetails</div>
+    <div>
+      {
+        apiResponse && apiResponse.map((e) => (
+          <DetailsMealsDrinks
+            key={ e.idDrink || e.idMeal }
+            recipe={ e }
+
+          />))
+      }
+
+    </div>
   );
 }
 
